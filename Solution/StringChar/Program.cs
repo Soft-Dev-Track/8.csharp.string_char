66fac5edb5c1a7b0f530736898e43cabbfecbcbf
Pierre <pierre.noel@becode.org>
2024-10-25 11:53:23 +0200

    update

---
+++ Solution/StringChar/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StringChar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Solution/TestStringChar/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TestStringChar
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
